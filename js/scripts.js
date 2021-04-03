fb97a4b4ac20c2411809b7dd96a40d0b8c0870c6
Fernando <69485703+fernd0@users.noreply.github.com>
2021-04-02 21:39:15 -0300

    att

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// $.get("https://ssl.geoplugin.net/json.gp?k=36a168c612a12e85", function (resp) {
+//     var data = JSON.parse(JSON.stringify(resp));
+//     $("#locationJS").html(data.geoplugin_city);
+// });
+
+$(document).ready(function(){setTimeout(function(){$("#button_chechout_hero").show();$("#button_chechout_final").show()}, 1470  * 1 * 1000)});
+
+ //1550
