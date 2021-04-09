3071a25bb0bac31116f6caa21a252d5df1ed6f55
Fernando <69485703+fernd0@users.noreply.github.com>
2021-04-09 19:33:09 -0300

    att

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-// $.get("https://ssl.geoplugin.net/json.gp?k=36a168c612a12e85", function (resp) {
-//     var data = JSON.parse(JSON.stringify(resp));
-//     $("#locationJS").html(data.geoplugin_city);
-// });
+$.get("https://ssl.geoplugin.net/json.gp?k=36a168c612a12e85", function (resp) {
+    var data = JSON.parse(JSON.stringify(resp));
+    $("#locationJS").html(data.geoplugin_city);
+});
 
 $(document).ready(function(){setTimeout(function(){$("#button_chechout_hero").show();$("#button_chechout_final").show()}, 1470  * 1 * 1000)});
 
