0e15e0967667b810fcfc607f9bf81d241e8a7f54
Fernando <69485703+fernd0@users.noreply.github.com>
2021-04-19 18:42:44 -0300

    att

--- js/scripts.js
+++ js/scripts.js
@@ -36,62 +36,6 @@ function notify(type, name, message){
 
 $(document).ready(function(){
 
-  var muteStatus;
-  var c = 0;
-  var nw = true;
-  var tag = document.createElement('script');
-  tag.src = "//www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  var player;*
-
-  function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-          videoId: "u49wjPa19Ug", //Change to the video ID you want
-          playerVars: {
-              autoplay: 0,
-              cc_load_policy: 0,
-              controls: 0,
-              disablekb: 1,
-              fs: 0,
-              playsinline: 1,
-              modestbranding: 1,
-              iv_load_policy: 3,
-              rel: 0,
-              showinfo: 0,
-              host: 'https://www.youtube.com'
-          },
-          events: {
-              onReady: onPlayerReady,
-              onStateChange: onPlayerStateChange,
-              onError: onPlayerError
-          }
-      });
-  }
-
-  function onPlayerStateChange(e) {
-    if (e.data === -1) {
-        c++;
-        if (c > 1) {
-            setTimeout(function() {
-                if (player.getPlayerState() === -1) {
-                    resetvid();
-                }
-            }, 3500);
-        }
-    }
-  }
-
-    if (e.data === 2) {
-      $(".cov").show();
-  }
-
-  $(".cov").click(function() {
-    player.playVideo();
-    $(".cov").hide();
-  });
-
-
     setTimeout(function(){
         $("#button_chechout_hero").show();
         $("#button_chechout_final").show()}, 1470  * 1 * 1000)
