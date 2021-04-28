01ebf8f5a5a128e3e36e234dfaad67f0c2819b9d
Fernando <69485703+fernd0@users.noreply.github.com>
2021-04-28 19:09:55 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,10 @@ function notify(type, name, message){
 $(document).ready(function(){
 
     setTimeout(function(){
+        $("#section-c").show();
+    }, 120000)
+
+    setTimeout(function(){
         $("#button_chechout_hero").show();
         $("#button_chechout_final").show()}, 1760000)
     });
