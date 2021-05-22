7abd7e706b7e3d2fbc8ce024064fcc783c574961
Fernando <69485703+fernd0@users.noreply.github.com>
2021-05-21 22:51:45 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -26,19 +26,19 @@ function notify(type, name, message){
   }
 
   setTimeout(function() {
-    notify("success", "🛒 Ana", " e outras 12 pessoas compraram este produto!");
+    notify("success", "Ana", " e outras 12 pessoas compraram este produto!");
   }, 1775000);
 
   setTimeout(function() {
-  	notify("success", "🛒 Luisa", " e outras 7 pessoas compraram este produto!");
+  	notify("success", "Luisa", " e outras 7 pessoas compraram este produto!");
   }, 1800000);
 
   setTimeout(function() {
-	  notify("success", "🛒 Maria", " e outras 24 pessoas compraram este produto!");
+	  notify("success", "Maria", " e outras 24 pessoas compraram este produto!");
   }, 1828800);
 
   setTimeout(function() {
-    notify("success", "🛒 Lilian", " e outras 18 pessoas compraram este produto!");
+    notify("success", "Lilian", " e outras 18 pessoas compraram este produto!");
   }, 2053800);
   
 
